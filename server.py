d93e63f540f3811f168b925299d6c6f12e2716c8
Peter Elliott <p.elliott332@gmail.com>
2018-01-06 20:09:30 -0700

    added git pull and process handling

--- server.py
+++ server.py
@@ -4,8 +4,11 @@ handles post requests from github webhooks
 import sys
 import http.server
 import json
+import subprocess
+import os
 
 instances = {}
+
 
 class WebhookHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
@@ -26,6 +29,23 @@ class WebhookHandler(http.server.BaseHTTPRequestHandler):
             return
         
         print("deploying", self.path)
+
+        if self.path in instances:
+            # properly kill all children of our process
+            subprocess.run(["pkill", "-TERM", "-P", str(instances[self.path].pid)])
+
+        if os.path.isdir("./"+self.path):
+            subprocess.run(["git", "-C", "./"+self.path, "pull", "git@github.com:{}.git".format(self.path)])
+        else:
+            subprocess.run(["git", "clone", "git@github.com:{}.git".format(self.path), "./"+self.path])
+        
+        if self.path in instances:
+            # make sure that the previous instance is done
+            instances[self.path].wait()
+        
+        instances[self.path] = subprocess.Popen(["sh", "-c", CONFIG[self.path]["script"]], cwd="./"+self.path)
+
+        print("deployed", self.path, "pid={}".format(instances[self.path].pid))
         self.send_response(200)
         self.end_headers()
  

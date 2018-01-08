a865b4334c69c30466ba84aa76a649836185fc41
Peter Elliott <p.elliott332@gmail.com>
2018-01-08 11:02:33 -0700

    integrated get_get_config, untested

--- deploy.py
+++ deploy.py
@@ -32,100 +32,76 @@ def get_get_config(path):
                 if "branch" not in proj:
                     proj["branch"] = "master"
 
+            if "port" not in config:
+                config["port"] = 80
+
         return config
     
     return get_config
 
 
+def make_handler(get_config):
+    instances = {}
 
-instances = {}
+    class WebhookHandler(http.server.BaseHTTPRequestHandler):
+        def do_POST(self): 
+            config = get_config()
+                        
+            if self.path not in config["projects"]:
+                self.send_response(404)
+                self.end_headers()
+                return
 
+            req_len = int(self.headers['Content-Length'])
+            req_bytes = self.rfile.read(req_len)
+            # the following line currently has no use, but will be used in the future
+            # req_data = json.loads(req_bytes.decode("utf-8"))
+            
+            if "secret" in config:
+                req_sig = self.headers["X-Hub-Signature"]
+                
+                if req_sig is None:
+                    print("rejecting deployment: no signature")
+                    self.send_response(403)
+                    self.end_headers()
+                    return
+                
+                hmac_bytes = hmac.new(config["secret"].encode("utf-8"), req_bytes, hashlib.sha1).digest()
+                hmac_sig = "sha1=" + hmac_bytes.hex()
 
-class WebhookHandler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        global CONFIG
-        global CONF_TIME
-        global instances
-        global GIT_ENV 
+                if not hmac.compare_digest(hmac_sig, req_sig):
+                    print("rejecting deployment: bad signature")
+                    self.send_response(403)
+                    self.end_headers()
+                    return
 
-        new_cnf_tme = os.path.getmtime(sys.argv[1])
-        if new_cnf_tme > CONF_TIME:
-            print("reloading config")
-            CONFIG = json.load(open(sys.argv[1]))
-            CONF_TIME = new_cnf_tme
+            print("deploying", self.path)
 
-            GIT_ENV = os.environ.copy()
-            if "ssh_key" in CONFIG:
-                GIT_ENV["GIT_SSH_COMMAND"] = "ssh -i " + CONFIG["ssh_key"]
- 
-        
-        if self.path not in CONFIG["projects"]:
-            self.send_response(404)
+            if os.path.isdir("./"+self.path):
+                subprocess.run(["git", "-C", "./"+self.path, "checkout", config[projects][self.path]["branch"]])
+                subprocess.run(["git", "-C", "./"+self.path, "pull", "git@github.com:{}.git".format(self.path)], env=config["git_env"])
+            else:
+                subprocess.run(["git", "clone", "git@github.com:{}.git".format(self.path), "./"+self.path], env=config["git_env"])
+                subprocess.run(["git", "-C", "./"+self.path, "checkout", config[projects][self.path]["branch"]])
+                subprocess.run(["git", "-C", "./"+self.path, "pull"], env=config["git_env"])
+            
+            if self.path in instances:
+                # kill the previous instance and make sure it completes
+                subprocess.run(["pkill", "-TERM", "-P", str(instances[self.path].pid)])
+                instances[self.path].wait()
+            
+            instances[self.path] = subprocess.Popen(config["projects"][self.path]["script"], cwd="./"+self.path, shell=True)
+
+            print("deployed", self.path, "pid={}".format(instances[self.path].pid))
+            self.send_response(200)
             self.end_headers()
-            return
 
-        req_len = int(self.headers['Content-Length'])
-        req_bytes = self.rfile.read(req_len)
-        # the following line currently has no use, but will be used in the future
-        # req_data = json.loads(req_bytes.decode("utf-8"))
-        
-        if "secret" in CONFIG:
-            req_sig = self.headers["X-Hub-Signature"]
-            
-            if req_sig is None:
-                print("rejecting deployment: no signature")
-                self.send_response(403)
-                self.end_headers()
-                return
-            
-            hmac_bytes = hmac.new(CONFIG["secret"].encode("utf-8"), req_bytes, hashlib.sha1).digest()
-            hmac_sig = "sha1=" + hmac_bytes.hex()
-
-            if not hmac.compare_digest(hmac_sig, req_sig):
-                print("rejecting deployment: bad signature")
-                self.send_response(403)
-                self.end_headers()
-                return
-
-        print("deploying", self.path)
-
-        if "branch" in CONFIG["projects"][self.path]:
-            branch = CONFIG["projects"][self.path]["branch"]
-        else:
-            branch = "master"
-
-        if os.path.isdir("./"+self.path):
-            subprocess.run(["git", "-C", "./"+self.path, "checkout", branch])
-            subprocess.run(["git", "-C", "./"+self.path, "pull", "git@github.com:{}.git".format(self.path)], env=GIT_ENV)
-        else:
-            subprocess.run(["git", "clone", "git@github.com:{}.git".format(self.path), "./"+self.path], env=GIT_ENV)
-            subprocess.run(["git", "-C", "./"+self.path, "checkout", branch])
-            subprocess.run(["git", "-C", "./"+self.path, "pull"], env=GIT_ENV)
-        
-        if self.path in instances:
-            # kill the previous instance and make sure it completes
-            subprocess.run(["pkill", "-TERM", "-P", str(instances[self.path].pid)])
-            instances[self.path].wait()
-        
-        instances[self.path] = subprocess.Popen(CONFIG["projects"][self.path]["script"], cwd="./"+self.path, shell=True)
-
-        print("deployed", self.path, "pid={}".format(instances[self.path].pid))
-        self.send_response(200)
-        self.end_headers()
- 
+    return WebHookHandler
+     
 
 if __name__ == "__main__":
-    CONFIG = json.load(open(sys.argv[1]))
-    CONF_TIME = os.path.getmtime(sys.argv[1])
-    
-    GIT_ENV = os.environ.copy()
-    if "ssh_key" in CONFIG:
-        GIT_ENV["GIT_SSH_COMMAND"] = "ssh -i " + CONFIG["ssh_key"]
-    
-    if "port" in CONFIG:
-        port = CONFIG["port"]
-    else:
-        port = 80
+    get_config = get_get_config(sys.argv[1])
+    WebhookHandler = make_handler(get_config) 
 
-    httpd = http.server.HTTPServer(('', port), WebhookHandler)
+    httpd = http.server.HTTPServer(('', get_config()["port"]), WebhookHandler)
     httpd.serve_forever()
